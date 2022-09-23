a5b903d50b9bef9c7e55f052be236dc8c4ad0e54
sgaje <sahilgajera12@gmail.com>
2022-09-23 10:04:31 +0530

    spalshscrn

--- app/src/main/java/com/example/pankajknitting/Welcome.java
+++ app/src/main/java/com/example/pankajknitting/Welcome.java
@@ -3,14 +3,16 @@ package com.example.pankajknitting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
-
+private Button wlcbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         getSupportActionBar().hide();
-        //hbhjbhjbjhbjmb  kjkjlkj
+        wlcbtn= findViewById(R.id.wlcbtn);
+
     }
 }
